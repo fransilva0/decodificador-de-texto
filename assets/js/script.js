232f0d0e03586701d5638ccd6bd7bd7ca56bd1df
Francileudo Oliveira <sfrancileudo1@gmail.com>
2022-08-11 15:45:00 -0300

    feat: animation added to the title and icon and decrypt function working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const BUTTON_ENCRYPT = document.getElementById('button-encrypt')
+const BUTTON_DECRYPT = document.getElementById('button-decrypt')
 
 BUTTON_ENCRYPT.onclick = () => {
   const TEXT_ENTRY = document.getElementById('input-area').value
@@ -30,4 +31,20 @@ BUTTON_ENCRYPT.onclick = () => {
   OUTPUT_AREA.style.justifyContent = 'start'
 
   OUTPUT_AREA_TEXT.innerHTML = ARRAY_LIST_ENCRYPT.join('')
+}
+
+BUTTON_DECRYPT.onclick = () => {
+  const TEXT_ENTRY = document.getElementById('input-area').value
+  const OUTPUT_AREA = document.getElementById('output-area')
+  const OUTPUT_AREA_TEXT = document.getElementById('output-area-text')
+  const HIDE_AREA = document.getElementById('hide-area')
+  const HIDE_AREA2 = document.getElementById('hide-area2')
+
+  const DECRYPT = TEXT_ENTRY.replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u')
+
+  HIDE_AREA.style.display = 'none'
+  HIDE_AREA2.style.display = 'none'
+  OUTPUT_AREA.style.justifyContent = 'start'
+
+  OUTPUT_AREA_TEXT.innerHTML = DECRYPT
 }
